f3d7133386428aeec2728f631a4906c0ed7a312a
ArinaFedotova <116386470+ArinaFedotova@users.noreply.github.com>
2023-11-16 22:54:41 +0300

    Delete IWriteFile.cs

--- IWriteFile.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Demographic.FileOperations
-{
-    public interface IWriteFile
-    {
-        void WriteData(string filename, string headers, Dictionary<string, List<int>> content);
-    }
-}
